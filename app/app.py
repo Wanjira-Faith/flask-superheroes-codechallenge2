8cf46ad19de8ef74a0cbd44d20f076b90e1b9da7
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-30 12:57:12 +0300

    set db URI to use sqlite and disable modification tracking

--- app/app.py
+++ app/app.py
@@ -2,3 +2,6 @@ from flask import Flask
 from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///superheroes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
