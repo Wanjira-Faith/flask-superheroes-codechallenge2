79c8eb55e6faa788af33bc0b70240ce597831aa7
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-30 13:18:27 +0300

    defined HeroPower model

--- app/models.py
+++ app/models.py
@@ -10,3 +10,10 @@ class Power(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), unique=True, nullable=False)
     description = db.Column(db.Text, nullable=False)
+
+class HeroPower(db.Model):
+    id = db.Column(db.Integer, primary=True)
+    hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'), nullable=False)
+    power_id = db.Column(db.Integer, db.ForeignKey('power.id'), nullable=False)
+
+    strength = db.Column(db.String(255), nullable=False)
