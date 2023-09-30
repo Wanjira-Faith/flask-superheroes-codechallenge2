e2495b929daaa16a2c86fa3fc31e718169b034b6
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-30 17:14:37 +0300

    defined route to get all heroes

--- app/app.py
+++ app/app.py
@@ -11,5 +11,19 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+# Route to get all heroes
+@app.route('/heroes', methods=['GET'])
+def get_heroes():
+    heroes = Hero.query.all()
+    hero_list = []
+    for hero in heroes:
+        hero_data = {
+            "id": hero.id,
+            "name": hero.name,
+            "super_name": hero.super_name
+        }
+        hero_list.append(hero_data)
+    return jsonify(hero_list)
+
 if __name__ == '__main__':
     app.run(debug=True)
