71461cc9f3e856291555449fcfbc7c46fd3c1a80
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-30 13:27:04 +0300

    defined the relationships

--- app/models.py
+++ app/models.py
@@ -6,14 +6,24 @@ class Hero(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), unique=True, nullable=False)
 
+    # One-to-Many relationship with HeroPower
+    hero_powers = db.relationship('HeroPower', backref='hero', lazy=True)
+
 class Power(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), unique=True, nullable=False)
     description = db.Column(db.Text, nullable=False)
 
+    # One-to-Many relationship with HeroPower
+    hero_powers = db.relationship('HeroPower', backref='power', lazy=True)
+
+
 class HeroPower(db.Model):
     id = db.Column(db.Integer, primary=True)
     hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'), nullable=False)
     power_id = db.Column(db.Integer, db.ForeignKey('power.id'), nullable=False)
-
     strength = db.Column(db.String(255), nullable=False)
+    
+    # Many-to-one relationships with Hero and Power
+    hero = db.relationship('Hero', back_populates='hero_powers')
+    power = db.relationship('Power', back_populates='hero_powers')
