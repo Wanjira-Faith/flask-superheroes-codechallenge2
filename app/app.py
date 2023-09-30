645865888616c99a410bfe32ab2aa3bdb26bc202
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-30 20:05:50 +0300

    defined route to post new hero-powers

--- app/app.py
+++ app/app.py
@@ -128,5 +128,70 @@ def update_power(id):
 
     return jsonify(updated_power_data), 200
 
+# Route to create new hero powers
+@app.route("/hero_powers", methods=["POST"])
+def create_hero_power():
+
+    # Validate request body
+    hero_power_data = request.get_json()
+
+    if "strength" not in hero_power_data:
+        return jsonify({
+            "errors": ["strength is required"]
+        }), 400
+
+    if "power_id" not in hero_power_data or not isinstance(hero_power_data["power_id"], int):
+        return jsonify({
+            "errors": ["power_id must be an integer"]
+        }), 400
+
+    if "hero_id" not in hero_power_data or not isinstance(hero_power_data["hero_id"], int):
+        return jsonify({
+            "errors": ["hero_id must be an integer"]
+        }), 400
+
+    # Check if the power exists
+    power = Power.query.get(hero_power_data["power_id"])
+    if not power:
+        return jsonify({
+            "errors": ["Power not found"]
+        }), 404
+
+    # Check if the hero exists
+    hero = Hero.query.get(hero_power_data["hero_id"])
+    if not hero:
+        return jsonify({
+            "errors": ["Hero not found"]
+        }), 404
+
+    # Create the new hero power
+    hero_power = HeroPower(
+        strength=hero_power_data["strength"],
+        power_id=hero_power_data["power_id"],
+        hero_id=hero_power_data["hero_id"]
+    )
+
+    # Save the new hero power
+    db.session.add(hero_power)
+    db.session.commit()
+
+    # Get hero data
+    hero_data = {
+        "id": hero.id,
+        "name": hero.name,
+        "super_name": hero.super_name,
+        "powers": []
+    }
+
+    for power in hero.powers:
+        hero_data["powers"].append({
+            "id": power.id,
+            "name": power.name,
+            "description": power.description
+        })
+
+    # Return the hero data
+    return jsonify(hero_data), 201
+
 if __name__ == '__main__':
     app.run(debug=True)
