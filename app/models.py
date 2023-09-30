4e0e73ccd38863b626eaac428e905bc91c3fc6c2
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-30 16:51:46 +0300

    added strength validations

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
 
@@ -26,6 +27,11 @@ class HeroPower(db.Model):
     power_id = db.Column(db.Integer, db.ForeignKey('power.id'), nullable=False)
     strength = db.Column(db.String(255), nullable=False)
     
-
     def __repr__(self):
         return f'<HeroPower {self.id}: Hero ID {self.hero_id}, Power ID {self.power_id}, Strength: {self.strength}>'
+    
+    @validates('strength')
+    def validate_strength(self, key, value):
+        if value not in ['Strong', 'Weak', 'Average']:
+            raise ValueError("Strength must be one of the following values: 'Strong', 'Weak', 'Average'")
+        return value
