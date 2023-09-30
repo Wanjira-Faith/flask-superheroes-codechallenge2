d9af92eb027ca74caefca7bf5dbd9c53cb905e42
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-30 13:03:23 +0300

    created a migrate object and initialized SQLAlchemy db

--- app/app.py
+++ app/app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///superheroes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
