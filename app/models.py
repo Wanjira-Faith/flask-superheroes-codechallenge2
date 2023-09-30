f310dc9ed71b3ebe82759bc2bd4f1a5dc012b124
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-30 16:53:05 +0300

    added description validations

--- app/models.py
+++ app/models.py
@@ -18,6 +18,12 @@ class Power(db.Model):
     name = db.Column(db.String(255), unique=True, nullable=False)
     description = db.Column(db.Text, nullable=False)
 
+    @validates('description')
+    def validate_description(self, key, value):
+        if len(value) < 20:
+            raise ValueError("Description must be at least 20 characters long")
+        return value
+
     def __repr__(self):
         return f'<Power {self.id}: {self.name}>'
 
@@ -27,11 +33,12 @@ class HeroPower(db.Model):
     power_id = db.Column(db.Integer, db.ForeignKey('power.id'), nullable=False)
     strength = db.Column(db.String(255), nullable=False)
     
-    def __repr__(self):
-        return f'<HeroPower {self.id}: Hero ID {self.hero_id}, Power ID {self.power_id}, Strength: {self.strength}>'
-    
     @validates('strength')
     def validate_strength(self, key, value):
         if value not in ['Strong', 'Weak', 'Average']:
             raise ValueError("Strength must be one of the following values: 'Strong', 'Weak', 'Average'")
         return value
+    
+    def __repr__(self):
+        return f'<HeroPower {self.id}: Hero ID {self.hero_id}, Power ID {self.power_id}, Strength: {self.strength}>'
+    
