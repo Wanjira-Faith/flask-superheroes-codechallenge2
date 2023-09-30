f6edecc056b3afb355ab857ab87a7e5c75586243
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-30 13:14:17 +0300

    defined power model

--- app/models.py
+++ app/models.py
@@ -5,3 +5,8 @@ db = SQLAlchemy()
 class Hero(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), unique=True, nullable=False)
+
+class Power(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+    description = db.Column(db.Text, nullable=False)
