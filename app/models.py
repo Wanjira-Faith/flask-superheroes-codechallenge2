4c0429940c7d2983687b81465d7c39e3a4208e97
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-30 13:11:53 +0300

    defined hero model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class Hero(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
