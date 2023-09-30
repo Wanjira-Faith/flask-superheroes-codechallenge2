a72833d899acc838760709ab2e207e6046f18c20
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-30 13:33:34 +0300

    created initial migration

---
+++ app/migrations/versions/db888f27ecbb_initial_migration.py
@@ -0,0 +1,24 @@
+"""Initial migration
+
+Revision ID: db888f27ecbb
+Revises: 
+Create Date: 2023-09-30 13:32:53.652879
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'db888f27ecbb'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
